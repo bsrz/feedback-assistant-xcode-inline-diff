80c6897946b0ef6727572f36425a986095b1a582
Ben Sarrazin <b@srz.io>
2023-02-06 15:07:06 -0500

    Adding empty project

---
+++ FeedbackAssistantXcodeInlineDiff/FeedbackAssistantXcodeInlineDiffApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackAssistantXcodeInlineDiffApp.swift
+//  FeedbackAssistantXcodeInlineDiff
+//
+//  Created by Ben Sarrazin on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackAssistantXcodeInlineDiffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
